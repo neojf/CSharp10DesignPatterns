de278bcbb8e249973461c4da9c1909413f64f60f
Jingfei Han <jinha@microsoft.com>
2022-05-24 09:11:30 +0800

    Singleton patterns

---
+++ Singleton/Implementation.cs
@@ -0,0 +1,35 @@
+﻿namespace Singleton
+{
+    /// <summary>
+    /// Singleton.
+    /// </summary>
+    public class Logger
+    {
+        private static Logger? _instance;
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+
+        protected Logger()
+        {
+        }
+
+        /// <summary>
+        /// SingletonOperation
+        /// </summary>
+        /// <param name="message"></param>
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using Singleton;
+
+Console.Title = "Singleton pattern";
+
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if (instance1 == instance2 && instance2 == Logger.Instance)
+{
+    Console.WriteLine("Instances are the same.");
+}
+
+instance1.Log($"Message from {nameof(instance1)}");
+instance1.Log($"Message from {nameof(instance2)}");
+Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+
+Console.ReadLine();
