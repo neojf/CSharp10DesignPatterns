0b5be0854d75e0e05425a1f2887c1db85042309b
Jingfei Han <jinha@microsoft.com>
2022-05-24 21:45:28 +0800

    add double check locking method to make thread safe

--- Singleton/Implementation.cs
+++ Singleton/Implementation.cs
@@ -1,7 +1,7 @@
 ﻿namespace Singleton
 {
     /// <summary>
-    /// Singleton.
+    /// Singleton logger using Lazy feature.
     /// </summary>
     public class Logger
     {
@@ -16,7 +16,47 @@
             }
         }
 
-        protected Logger()
+        private Logger()
+        {
+        }
+
+        /// <summary>
+        /// SingletonOperation
+        /// </summary>
+        /// <param name="message"></param>
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
+
+    /// <summary>
+    /// Singleton logger using double checked locking.
+    /// </summary>
+    public class Logger2
+    {
+        private static Logger2? _instance;
+        private static readonly object _lock = new object();
+
+        public static Logger2 Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock(_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Logger2();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private Logger2()
         {
         }
 
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -14,4 +14,15 @@ instance1.Log($"Message from {nameof(instance1)}");
 instance1.Log($"Message from {nameof(instance2)}");
 Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
 
+var instance3 = Logger2.Instance;
+var instance4 = Logger2.Instance;
+if (instance3 == instance4 && instance4 == Logger2.Instance)
+{
+    Console.WriteLine("Instances are the same.");
+}
+
+instance3.Log($"Message from {nameof(instance3)}");
+instance4.Log($"Message from {nameof(instance4)}");
+Logger2.Instance.Log($"Message from {nameof(Logger2.Instance)}");
+
 Console.ReadLine();
