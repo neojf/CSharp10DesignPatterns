0ff7c36e1b1ffe8cc8d629b40fc083b9310c5084
Jingfei Han <jinha@microsoft.com>
2022-06-05 09:40:28 +0800

    Add abstract factory example

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,18 @@
+﻿using AbstractFactory;
+
+Console.Title = "Abstract Factory";
+
+FurnitureFactory factory = new VictorianFurnitureFactory();
+Client client = new Client(factory);
+client.DoSomeOperation();
+Console.WriteLine("***********************************");
+
+FurnitureFactory factory1 = new ArtDecoFurnitureFactory();
+Client client1 = new Client(factory1);
+client1.DoSomeOperation();
+Console.WriteLine("***********************************");
+
+FurnitureFactory factory2 = new ModernFurnitureFactory();
+Client client2 = new Client(factory2);
+client1.DoSomeOperation();
+Console.WriteLine("***********************************");
